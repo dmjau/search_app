002f2cb29f68de05351746aac28c07c93231b31a
dmjau <dmjaume.bsas@gmail.com>
2023-10-13 16:21:02 -0300

    changed class type

--- app/src/main/java/com/mercadolibre/pipsearch/android/app/data/model/ScreenItemsDto.kt
+++ app/src/main/java/com/mercadolibre/pipsearch/android/app/data/model/ScreenItemsDto.kt
@@ -3,7 +3,7 @@ package com.mercadolibre.pipsearch.android.app.data.model
 import com.mercadolibre.android.commons.serialization.annotations.Model
 
 @Model
-class ScreenItemsDto(
+data class ScreenItemsDto(
     val query: String,
     val results: List<ProductDto>
 )
