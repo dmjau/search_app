be5705567288715815901394b55328f2073578ee
dmjau <dmjaume.bsas@gmail.com>
2023-10-17 16:25:08 -0300

    fix tests

--- app/src/main/java/com/mercadolibre/pipsearch/android/app/data/model/ScreenItemsDto.kt
+++ app/src/main/java/com/mercadolibre/pipsearch/android/app/data/model/ScreenItemsDto.kt
@@ -5,5 +5,5 @@ import com.mercadolibre.android.commons.serialization.annotations.Model
 @Model
 data class ScreenItemsDto(
     val query: String,
-    val results: List<ProductDto>
+    val results: List<ItemDto>
 )
--- app/src/test/java/com/mercadolibre/pipsearch/android/app/data/model/ScreenItemsDtoTest.kt
+++ app/src/test/java/com/mercadolibre/pipsearch/android/app/data/model/ScreenItemsDtoTest.kt
@@ -5,28 +5,27 @@ import org.junit.Test
 
 class ScreenItemsDtoTest {
 
-
     @Test
     fun `Screen Items DTO parses ok`() {
         // given
         val listOfTags = listOf("tag_1_test", "tag_1_test", "tag_1_test")
-        val itemTest1 = ProductDto(
+        val itemTest1 = ItemDto(
             "itemTest 1",
             1111.0,
             "https://test_image_item_test_1.jpg",
             listOfTags
         )
-        val itemTest2 = ProductDto(
+        val itemTest2 = ItemDto(
             "itemTest 2",
             2222.0,
             "https://test_image_item_test_2.jpg",
             listOfTags
         )
-        val listOfProductsTest = listOf(itemTest1, itemTest2)
-        val screenTest = ScreenItemsDto("search test", listOfProductsTest)
+        val listOfItemsTest = listOf(itemTest1, itemTest2)
+        val screenTest = ScreenItemsDto("screen items dto test", listOfItemsTest)
 
         // then
-        assertEquals("search test", screenTest.query)
+        assertEquals("screen items dto test", screenTest.query)
         assertEquals(itemTest1, screenTest.results[0])
         assertEquals(itemTest2, screenTest.results[1])
     }
