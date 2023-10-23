65a086e32677612144134289681e896b2b523e76
dmjau <dmjaume.bsas@gmail.com>
2023-10-22 21:23:52 -0300

    added test

--- app/src/test/java/com/mercadolibre/pipsearch/android/app/MainActivityTest.kt
+++ app/src/test/java/com/mercadolibre/pipsearch/android/app/MainActivityTest.kt
@@ -1,12 +1,13 @@
 package com.mercadolibre.pipsearch.android.app
 
-import androidx.lifecycle.ViewModelProvider
+import com.mercadolibre.android.andesui.searchbox.AndesSearchbox
 import com.mercadolibre.pipsearch.android.app.ui.view.MainActivity
 import com.mercadolibre.pipsearch.android.app.ui.view.MainViewModel
 import com.mercadolibre.pipsearch.android.databinding.PipSearchAppMainActivityBinding
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -47,19 +48,29 @@ class MainActivityTest {
     }
 
     @Test
+    fun testMainActivityBindingSetListener() {
+        // given
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
+        val reflectionActivityBinding = ReflectionHelpers.getField<PipSearchAppMainActivityBinding>(activity, "binding")
+
+        // then
+        assertNotNull(reflectionActivityBinding.pipMainHeaderSearchbox.onSearchListener)
+        assertTrue(reflectionActivityBinding.pipMainHeaderSearchbox.onSearchListener is AndesSearchbox.OnSearchListener)
+    }
+
+    @Test
     fun testMainActivityTextToSearch() {
         // given
         val mockViewModel = mockk<MainViewModel>(relaxed = true)
-
         val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
 
         ReflectionHelpers.setField(activity, "mainViewModel", mockViewModel)
 
         val text = "test text"
-
         val sendTextToSearchMethod = activity.javaClass.getDeclaredMethod("sendTextToSearch", String::class.java)
         sendTextToSearchMethod.isAccessible = true
 
+        // when
         sendTextToSearchMethod.invoke(activity, text)
 
         // then
@@ -72,16 +83,15 @@ class MainActivityTest {
     fun testMainActivityTextToSearchWithLongText() {
         // given
         val mockViewModel = mockk<MainViewModel>(relaxed = true)
-
         val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
 
         ReflectionHelpers.setField(activity, "mainViewModel", mockViewModel)
 
         val text = "This is a long text with 100 caracters for test function send to text in the view model and probe how it is work in this use case"
-
         val sendTextToSearchMethod = activity.javaClass.getDeclaredMethod("sendTextToSearch", String::class.java)
         sendTextToSearchMethod.isAccessible = true
 
+        // when
         sendTextToSearchMethod.invoke(activity, text)
 
         // then
