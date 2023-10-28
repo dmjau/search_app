a138f9491f194cfbccd45a25c5264592d77291eb
dmjau <dmjaume.bsas@gmail.com>
2023-10-27 21:26:24 -0300

    create class and xml

---
+++ app/src/main/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivity.kt
@@ -0,0 +1,14 @@
+package com.mercadolibre.pipsearch.android.app.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mercadolibre.pipsearch.android.R
+
+class CartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pip_search_app_cart_activity)
+    }
+
+}
