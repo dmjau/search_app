84b1228c1bb56114a486675ee1c21cdda4270c9c
dmjau <dmjaume.bsas@gmail.com>
2023-10-29 14:39:44 -0300

    added binding

--- app/src/main/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivity.kt
+++ app/src/main/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivity.kt
@@ -3,12 +3,21 @@ package com.mercadolibre.pipsearch.android.app.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mercadolibre.pipsearch.android.R
+import com.mercadolibre.pipsearch.android.databinding.PipSearchAppCartActivityBinding
 
 class CartActivity : AppCompatActivity() {
 
+    companion object {
+        const val TITLE_HEADER_CART = "Carrito"
+    }
+
+    private var binding: PipSearchAppCartActivityBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pip_search_app_cart_activity)
+
+        binding = PipSearchAppCartActivityBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
     }
 
 }
--- app/src/test/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivityTest.kt
+++ app/src/test/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivityTest.kt
@@ -1,10 +1,12 @@
 package com.mercadolibre.pipsearch.android.app.ui.view
 
 import androidx.test.core.app.launchActivity
+import com.mercadolibre.pipsearch.android.databinding.PipSearchAppCartActivityBinding
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.util.ReflectionHelpers
 
 @RunWith(RobolectricTestRunner::class)
 class CartActivityTest {
@@ -16,5 +18,16 @@ class CartActivityTest {
 
         // then
         assertNotNull(cartActivity)
+    }
+
+    @Test
+    fun testCartActivityInstanceBinding() {
+        launchActivity<CartActivity>().onActivity { activity ->
+            val reflectionBinding = ReflectionHelpers.getField<PipSearchAppCartActivityBinding>(activity, "binding")
+
+            // then
+            assertNotNull(reflectionBinding)
+            assertNotNull(reflectionBinding.root)
+        }
     }
 }
