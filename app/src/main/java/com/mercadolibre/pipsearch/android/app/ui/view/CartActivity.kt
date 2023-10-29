ba09641abd73262d7e0c707e9a9cfc0f1ce95d38
dmjau <dmjaume.bsas@gmail.com>
2023-10-29 15:00:24 -0300

    create tests and added methods to build base screen

--- app/src/main/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivity.kt
+++ app/src/main/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivity.kt
@@ -2,13 +2,15 @@ package com.mercadolibre.pipsearch.android.app.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mercadolibre.pipsearch.android.R
+import android.view.View
 import com.mercadolibre.pipsearch.android.databinding.PipSearchAppCartActivityBinding
 
 class CartActivity : AppCompatActivity() {
 
     companion object {
-        const val TITLE_HEADER_CART = "Carrito"
+        const val CART_HEADER_TITLE = "Carrito"
+        const val CART_BODY_TITLE = "El carrito está vacío"
+        const val CART_BODY_SUBTITLE = "Volvé a la pantalla de principal para buscar ítems."
     }
 
     private var binding: PipSearchAppCartActivityBinding? = null
@@ -18,6 +20,37 @@ class CartActivity : AppCompatActivity() {
 
         binding = PipSearchAppCartActivityBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        setTitleHeader()
+        initBaseScreen()
+    }
+
+    /**
+     * Set base screen with initial title before any search.
+     */
+    private fun setTitleHeader() {
+        binding!!.pipCartHeaderText.text = CART_HEADER_TITLE
+    }
+
+    /**
+     * Set base screen with initial title before any search.
+     */
+    private fun initBaseScreen() {
+        showBaseScreenHideRecyclerView()
+        setBaseTitle(CART_BODY_TITLE)
+        setBaseSubtitle(CART_BODY_SUBTITLE)
+    }
+
+    private fun setBaseTitle(title: String) {
+        binding!!.pipCartBodyTitle.append(title)
+    }
+    private fun setBaseSubtitle(subtitle: String) {
+        binding!!.pipCartBodySubtitle.append(subtitle)
+    }
+
+    private fun showBaseScreenHideRecyclerView() {
+        binding!!.pipCartBodyRecyclerContainer.visibility = View.GONE
+        binding!!.pipCartBodyImageContainer.visibility = View.VISIBLE
     }
 
 }
--- app/src/test/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivityTest.kt
+++ app/src/test/java/com/mercadolibre/pipsearch/android/app/ui/view/CartActivityTest.kt
@@ -1,7 +1,10 @@
 package com.mercadolibre.pipsearch.android.app.ui.view
 
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.test.core.app.launchActivity
 import com.mercadolibre.pipsearch.android.databinding.PipSearchAppCartActivityBinding
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,6 +31,29 @@ class CartActivityTest {
             // then
             assertNotNull(reflectionBinding)
             assertNotNull(reflectionBinding.root)
+        }
+    }
+
+    @Test
+    fun testCartActivitySetBaseScreenTexts() {
+        launchActivity<CartActivity>().onActivity { activity ->
+            val reflectionBinding = ReflectionHelpers.getField<PipSearchAppCartActivityBinding>(activity, "binding")
+
+            // then
+            assertEquals("Carrito", reflectionBinding.pipCartHeaderText.text.toString())
+            assertEquals("El carrito está vacío", reflectionBinding.pipCartBodyTitle.text.toString())
+            assertEquals("Volvé a la pantalla de principal para buscar ítems.", reflectionBinding.pipCartBodySubtitle.text.toString())
+        }
+    }
+
+    @Test
+    fun testCartActivityShowBaseScreenAndHideReclerView() {
+        launchActivity<CartActivity>().onActivity { activity ->
+            val reflectionBinding = ReflectionHelpers.getField<PipSearchAppCartActivityBinding>(activity, "binding")
+
+            // then
+            assertEquals(GONE, reflectionBinding.pipCartBodyRecyclerContainer.visibility)
+            assertEquals(VISIBLE, reflectionBinding.pipCartBodyImageContainer.visibility)
         }
     }
 }
