286f32e2986f5f5bd1fa7688854500ba72c4bfac
dmjau <dmjaume.bsas@gmail.com>
2023-11-22 16:12:32 -0300

    added test

--- app/src/test/java/com/mercadolibre/pipsearch/android/app/domain/CartManagerTest.kt
+++ app/src/test/java/com/mercadolibre/pipsearch/android/app/domain/CartManagerTest.kt
@@ -2,10 +2,16 @@ package com.mercadolibre.pipsearch.android.app.domain
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.mercadolibre.pipsearch.android.app.data.model.ItemDto
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -16,6 +22,14 @@ class CartManagerTest {
     @Rule
     @JvmField
     val instantExecutorRule = InstantTaskExecutorRule()
+
+    @MockK(relaxed = true)
+    private lateinit var mockObserver: Observer<MutableList<ItemDto>>
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
 
     @Test
     fun TestGetInstanceReturnTheSameInstance() {
@@ -36,6 +50,8 @@ class CartManagerTest {
         val mockMutableListOfItems: MutableList<ItemDto> = mutableListOf()
 
         val cartManager = CartManager.getInstance()
+        val resetMutableLiveData: MutableLiveData<MutableList<ItemDto>> = MutableLiveData()
+        ReflectionHelpers.setField(cartManager, "_itemsOnCart", resetMutableLiveData)
 
         var reflectionListItemsOnCart =
             ReflectionHelpers.getField<MutableLiveData<MutableList<ItemDto>>>(cartManager, "_itemsOnCart")
@@ -50,5 +66,39 @@ class CartManagerTest {
 
         // then
         assertEquals(1, reflectionListItemsOnCart.value!!.size)
+    }
+
+    @Test
+    fun TestCartManagerCallUpdateItemList() {
+
+        // given
+        val mockItem1 = ItemDto("Item 1", 10.0, "test_1", emptyList())
+        val mockMutableListOfItems: MutableList<ItemDto> = mutableListOf()
+
+        val cartManager = CartManager.getInstance()
+
+        cartManager.itemsOnCart.observeForever(mockObserver)
+
+        var reflectionListItemsOnCart =
+            ReflectionHelpers.getField<MutableLiveData<MutableList<ItemDto>>>(cartManager, "_itemsOnCart")
+
+        assertNull(reflectionListItemsOnCart.value)
+
+        // when
+        mockMutableListOfItems.add(mockItem1)
+        cartManager.updateItemList(mockMutableListOfItems)
+
+        reflectionListItemsOnCart = ReflectionHelpers.getField(cartManager, "_itemsOnCart")
+
+        // then
+        assertEquals(1, reflectionListItemsOnCart.value!!.size)
+        assertTrue(cartManager.itemsOnCart.value == mockMutableListOfItems)
+
+        verify(exactly = 1) {
+            mockObserver.onChanged(mockMutableListOfItems)
+        }
+
+        // remove observer
+        cartManager.itemsOnCart.removeObserver(mockObserver)
     }
 }
