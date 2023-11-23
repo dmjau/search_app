3eb85616793833daa3fedc92d2aa315824ee43bf
dmjau <dmjaume.bsas@gmail.com>
2023-11-23 12:35:58 -0300

    refactor cart view model

--- app/src/main/java/com/mercadolibre/pipsearch/android/app/ui/view/viewmodels/CartViewModel.kt
+++ app/src/main/java/com/mercadolibre/pipsearch/android/app/ui/view/viewmodels/CartViewModel.kt
@@ -1,5 +1,6 @@
 package com.mercadolibre.pipsearch.android.app.ui.view.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mercadolibre.pipsearch.android.app.data.model.ItemDto
@@ -8,7 +9,7 @@ class CartViewModel : ViewModel() {
 
     private val _itemsOnCart: MutableLiveData<List<ItemDto>> = MutableLiveData()
 
-    val itemsOnCart: MutableLiveData<List<ItemDto>> = _itemsOnCart
+    val itemsOnCart: LiveData<List<ItemDto>> = _itemsOnCart
 
     fun updateItemList(itemList: List<ItemDto>) {
         _itemsOnCart.value = itemList
